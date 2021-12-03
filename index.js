877332a279cf62712bed1dfbb86410d242cae00f
Narczo12 <95502409+Narczo12@users.noreply.github.com>
2021-12-03 21:13:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require("fs");
 const config = require('./config.json')
+const dotenv = require('dotenv').config()
 client.commands = new Discord.Collection();
 
 
@@ -146,4 +147,4 @@ fs.readdir("./cmd/", (err, files) => {
   
   })
 
-client.login(config.token)
+client.login(process.env.token)
